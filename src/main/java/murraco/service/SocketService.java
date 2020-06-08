e417df0fe71f15536d43b6eb0210810c47f69328
Than Trung Hieu <hieu19926@gmail.com>
2020-06-08 15:00:38 +0700

    format code

--- src/main/java/murraco/service/SocketService.java
+++ src/main/java/murraco/service/SocketService.java
@@ -4,13 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import murraco.model.Command;
 import murraco.model.EchoModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.Date;
 
 /**
